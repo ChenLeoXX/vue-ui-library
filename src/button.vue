8164894677edf5d26d5d7e0057bf4a4cb4e0d8df
Leo <479711970@qq.com>
2019-02-12 00:04:16 +0800

    添加按钮ICON 和 部分按钮样式 开放部分按钮接口：类型，图标

--- src/button.vue
+++ src/button.vue
@@ -1,18 +1,72 @@
 <template>
-   <div class="container">
-     <button class="v-button">测试按钮
+     <button class="v-button" :class="[type]">
+       <slot></slot>
+       <v-icon v-if="icon" :icon-name="icon"></v-icon>
      </button>
-   </div>
 </template>
-
 <script>
+    import vIcon from './v-icon';
     export default {
+        props:{
+            icon:{
+                type:String,
+            },
+            loading:{
+                type:Boolean,
+                default: false,
+            },
+            type:{
+                type:String,
+                default:'default'
+            }
+        },
+        components:{
+            vIcon,
+        }
     }
 </script>
 <style lang="scss">
-    .container {
-      .v-button {
-          background: palegoldenrod;
-      }
+  .v-button {
+    min-height: var(--button-height);
+    font-size: var(--font-size);
+    color: var(--button-color);
+    border-radius: var(--border-radius);
+    padding: 0 1em;
+    transition: all .2s ease-in;
+    &.primary {
+      background-color: var(--primary);
+      border:var(--primary);
+      outline:var(--primary);
     }
+    &.warn {
+      background-color: var(--warn);
+      border:var(--warn);
+      outline:var(--wran);
+    }
+    &.error {
+      background-color: var(--error);
+      border:var(--error);
+      outline:var(--error);
+    }
+    &.success {
+      background-color: var(--success);
+      border:var(--success);
+      outline:var(--success);
+    }
+    &.default {
+      background-color: #fff;
+      border-width: 1px;
+      border-style: solid;
+      border-color: #dcdee2;
+      outline:none;
+      color:var(--text-color)
+    }
+    &:not(.default):hover {
+      opacity: 0.8;
+    }
+    &.default:hover {
+      color: var(--primary);
+      border-color: var(--primary);
+    }
+  }
 </style>
---
+++ src/v-icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="v-icon">
+    <use :xlink:href=`#i-${iconName}`></use>
+  </svg>
+</template>
+<script>
+  export default {
+      props:{
+          iconName:{
+              type:String,
+              default:'settings'
+          }
+      }
+  }
+</script>
+<style lang="scss">
+  .v-icon {
+    width: 1em; height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
