21f59f9d5d22e4a60037c2c3157b86c474b44804
ChenLeoXX <479711970@qq.com>
2019-03-16 18:57:58 +0800

    创建button group组件

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import button from './button'
+import VIcon from './v-icon'
+import buttonGroup from './buttton-group'
 Vue.component('v-button',button);
+Vue.component('v-icon',VIcon);
+Vue.component('button-group',buttonGroup);
 new Vue({
     el:"#app",
     data :{
---
+++ src/buttton-group.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+    mounted(){
+        for(let node of this.$el.children) {
+            if(node.tagName.toLocaleLowerCase() !== 'button'){
+                console.warn('invalid slot: the slot node must be button')
+            }
+        }
+    },
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="scss">
+  .button-group {
+    display: inline-flex;
+    vertical-align: top;
+    .v-button {
+      border-radius: 0;
+      margin-left:-1px;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
