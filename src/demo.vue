1b81320f3c0788e3d02244cd95caadf4ebedb88f
leo <479711970@QQ.com>
2019-08-17 15:30:07 +0800

    改用vue cli 构建

---
+++ src/demo.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="demo">
+		<v-button>按钮</v-button>
+	</div>
+</template>
+<script>
+    import button from './button'
+
+    export default {
+        name: 'demo',
+        data() {
+            return {}
+        },
+        components: {
+            'v-button': button
+        }
+    }
+</script>
+<style>
+	*, *::before, *::after {
+		box-sizing: border-box;
+	}
+	
+	body {
+		margin: 0;
+		font-size: 14px;
+		padding: 150px;
+	}
+</style>
