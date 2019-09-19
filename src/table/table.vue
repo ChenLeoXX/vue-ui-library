e0d3d50323001878963482fc01cb004c8a1dde18
leo <479711970@QQ.com>
2019-09-20 00:18:14 +0800

    table基本样式

--- src/table/table.vue
+++ src/table/table.vue
@@ -3,18 +3,39 @@
 		<table class="v-table">
 			<thead>
 			<tr>
-				<th v-for="item in columns" :key="item.field">{{item.name}}</th>
+				<th v-if="showNum">
+					<span>No</span>
+				</th>
+				<th class="selection">
+					<label>
+						<input type="checkbox">
+					</label>
+				</th>
+				<th v-for="item in columns" :key="item.field">
+					<span class="th-content">
+						{{item.name}}
+					</span>
+				</th>
 			</tr>
 			</thead>
 			<tbody>
-			<tr v-for="item in dataSource" :key="item.key">
-				<td>
+			<tr v-for="(item,index) in dataSource" :key="item.key">
+				<td v-if="showNum">
+					<span class="td-content">
+						{{index+1}}
+					</span>
+				</td>
+				<td class="selection">
 					<label>
 						<input type="checkbox">
 					</label>
 				</td>
 				<template>
-					<td v-for="head in columns" :key="head.field">{{item[head.field]}}</td>
+					<td v-for="head in columns" :key="head.field">
+						<span class="td-content">
+							{{item[head.field]}}
+						</span>
+					</td>
 				</template>
 			</tr>
 			</tbody>
@@ -30,6 +51,10 @@
 
         },
         props: {
+            showNum: {
+                type: Boolean,
+                default: false
+            },
             selectedItems: {
                 type: Array,
                 default: () => []
@@ -51,10 +76,50 @@
 </script>
 
 <style lang="scss" scoped>
+	@import "../../style/var";
 	.v-table-wrapper {
 		.v-table {
-			td {
-				border: 1px solid red
+			width: 100%;
+			text-align: left;
+			border-collapse: separate;
+			border-spacing: 0;
+			
+			tbody {
+				tr {
+					transition: all .3s;
+				}
+				
+				tr:hover {
+					td {
+						background: $td-hover;
+					}
+				}
+			}
+			
+			td, th {
+				border-bottom: 1px solid $gray-border;
+				padding: 16px;
+				background: #fff;
+			}
+			
+			th {
+				background: #fafafa;
+				font-weight: 500;
+				
+				&:first-child, &:last-child {
+					border-top-left-radius: $border-radius;
+				}
+			}
+			
+			.selection {
+				width: 60px;
+				text-align: center;
+				
+				input[type="checkbox"] {
+					width: 16px;
+					height: 16px;
+					margin: 0;
+				}
 			}
 		}
 	}
